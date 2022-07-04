89ab3fb38d40a555c4b45564647a996778c9c72f
CYTsai <47322522+cytsai1008@users.noreply.github.com>
2022-07-04 19:49:46 +0000

    Add regex to avoid wrong address, enter event listener for auto run

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,19 +24,26 @@ function processor(buttonElement, nameElement, addrElement) {
 
     // lock the button to prevent duplicated actions
     buttonElement.disabled = true;
+    const RegPat = /https?:\/\/(?:www\.|(?!www))[a-zA-Z\d][a-zA-Z\d-]+[a-zA-Z\d]\.\S{2,}|www\.[a-zA-Z\d][a-zA-Z\d-]+[a-zA-Z\d]\.\S{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z\d]+\.\S{2,}|www\.[a-zA-Z\d]+\.\S{2,}|[a-zA-Z\d][a-zA-Z\d-]+[a-zA-Z\d]\.\S{2,}/gi
+    if (!RegPat.test(document.getElementById("project-addr").value)) {
+        window.alert("非許可連結 | INVAILDED LINK")
+        buttonElement.disabled = false;
+    }
+    else {
 
-    // create a blob
-    const blob = createBlob(createPage(name, addr), "text/html");
-    // and redirect users to the blob
-    /// create the link
-    const link = URL.createObjectURL(blob);
-    /// redirect, and
-    redirectToObjectURL(link, name);
-    /// revoke the object url to release the memory
-    URL.revokeObjectURL(link);
+        // create a blob
+        const blob = createBlob(createPage(name, addr), "text/html");
+        // and redirect users to the blob
+        /// create the link
+        const link = URL.createObjectURL(blob);
+        /// redirect, and
+        redirectToObjectURL(link, name);
+        /// revoke the object url to release the memory
+        URL.revokeObjectURL(link);
 
-    // release the button
-    buttonElement.disabled = false;
+        // release the button
+        buttonElement.disabled = false;
+    }
 }
 
 function processorWrapper(buttonElement, nameElement, addrElement) {
@@ -49,6 +56,15 @@ function main() {
     const downloadBtn = document.getElementById("download-code");
 
     downloadBtn.addEventListener("click", processorWrapper(downloadBtn, projectName, projectAddr));
+    projectAddr.addEventListener("keypress", function(event) {
+        // If the user presses the "Enter" key on the keyboard
+        if (event.key === "Enter") {
+          // Cancel the default action, if needed
+          event.preventDefault();
+          // Trigger the button element with a click
+          document.getElementById("download-code").click();
+        }
+      });
 }
 
 main()
