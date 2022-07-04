cc0c324c71622c3f6740cf7e3ee677682e9b9854
CYTsai <47322522+cytsai1008@users.noreply.github.com>
2022-07-04 08:24:58 +0000

    Update for my own purpose usage

--- assets/templates/template.js
+++ assets/templates/template.js
@@ -8,12 +8,18 @@ function createPage(projectName, projectAddress) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${projectName}</title>
     <!-- Generate with "Generate a redirector!" -->
+    <style>
+    body{
+        text-align: center;
+        font-family: Tahoma, Verdana, sans-serif;
+    }
+    </style>
 </head>
 <body>
     <p>如果未重新導向，請開啟 JavaScript 或直接存取以下連結。</p>
     <p>If it is not redirected, enable JavaScript or visit the following address.</p>
-    <p>${projectAddress}</p>
-    <p><small>Generated with <a href="https://epf-redirector.pan93.tk">Generate a (EPF) redirector!</a>. (C) 2021, pan93412.</small></p>
+    <a href="${projectAddress}">${projectAddress}</a>
+    <p><small>Generated with <a href="https://epf-redirector.pan93.tk">Generate a (EPF) redirector!</a>. <br \\>(C) 2021, pan93412 <br \\>(C) 2022, cytsai1008 Maintainced</small></p>
 
     <!-- compatible for every browsers, I thought. -->
     <script><!--
